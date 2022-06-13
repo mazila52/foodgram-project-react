982c3877e6188e86621c7150de06f2a8f35b335d
mazila52 <yaserenkii@yandex.ru>
2022-06-13 15:25:44 +0300

    13/06/22

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,9 +7,9 @@ router = DefaultRouter()
 router.register(r'recipes',RecipeViewSet, basename='recipes')
 urlpatterns = [
     path('', include('djoser.urls')),
-    #path('', include('router.urls')),
+    path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
     path('tags/', TagList.as_view()), 
     path('tags/<int:pk>/', TagDetail.as_view()),
-    path('recipes/', include(router.urls)),    
+    #path('recipes/', include(router.urls)),    
 ]
